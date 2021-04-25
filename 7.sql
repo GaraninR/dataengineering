59d0c5aa65f1da6879c08f485b0093fce9888b6c
Roman <garanin.r@gmail.com>
2021-04-26 00:58:01 +0300

    Fix request for 7 subtask

---
+++ 7.sql
@@ -0,0 +1,23 @@
+with sum_all as (
+	select sum((EXTRACT(EPOCH FROM r.return_date) - EXTRACT(EPOCH FROM r.rental_date))/3600) sum_hours, ct.city city, cat."name" category from rental r
+	join customer c
+	on r.customer_id  = c.customer_id
+	join address a
+	on a.address_id  = c.address_id 
+	join city ct
+	on a.city_id  = ct.city_id 
+	join inventory i 
+	on r.inventory_id = i.inventory_id 
+	join film f
+	on f.film_id = i.film_id
+	join film_category fc 
+	on fc.category_id = f.film_id 
+	join category cat
+	on cat.category_id = fc.category_id
+	where upper(ct.city) like 'A%' or ct.city like '%-%'
+	group by ct.city, cat."name" 
+) 
+select sum(coalesce(sum_hours, 0)) sum_fin, category from sum_all
+group by category
+order by sum_fin desc
+limit 1;
